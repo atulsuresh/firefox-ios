99962cd22c935b6a23fa7a940cb36886e0b06753
Richard Newman <rnewman@twinql.com>
2014-12-10 19:29:57 +0000

    WIP

--- Client/DataAccess/Bookmarks.swift
+++ Client/DataAccess/Bookmarks.swift
@@ -4,17 +4,106 @@
 
 import Foundation
 
-public protocol Bookmarks {
+import UIKit
 
-}
+private let BOOKMARK_CELL_IDENTIFIER = "BOOKMARK_CELL"
+private let BOOKMARK_HEADER_IDENTIFIER = "BOOKMARK_HEADER"
 
 // Lots of work needed on this.
-class BookmarkItem {
+public class BookmarkItem {
     var title: String
     var url: String
+
+    var icon: UIImage {
+        return createMockFavicon(UIImage(named: "leaf.png")!);
+    }
 
     init(title: String, url: String) {
         self.title = title
         self.url = url
+    }
+}
+
+public protocol Bookmarks {
+    var count: Int { get };
+    func get(index: Int) -> BookmarkItem?;
+
+    func tableDataSource() -> UITableViewDataSource;
+}
+
+private class StubBookmarksUITableViewDataSource: NSObject, UITableViewDataSource {
+    private var bookmarks: Bookmarks;
+
+    init(bookmarks: Bookmarks) {
+        self.bookmarks = bookmarks;
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section != 0) {
+            return 0;
+        }
+
+        return self.bookmarks.count;
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(BOOKMARK_CELL_IDENTIFIER, forIndexPath: indexPath) as UITableViewCell;
+
+        cell.textLabel?.textColor = UIColor.darkGrayColor();
+        cell.indentationWidth = 20;
+        cell.textLabel?.font = UIFont(name: "FiraSans-SemiBold", size: 13);
+
+        if let bookmark = self.bookmarks.get(indexPath.row) {
+            cell.textLabel?.text = bookmark.title;
+            cell.imageView?.image = bookmark.icon;
+
+            /*
+            // TODO: We need an async image loader api here
+            favicons.getForUrl(NSURL(string: bookmark.url)!, options: nil, callback: { (icon: Favicon) -> Void in
+            if let img = icon.img {
+            cell.imageView?.image = createMockFavicon(img);
+            }
+            });
+            */
+        } else {
+            cell.textLabel?.text = NSLocalizedString("No bookmark", comment: "Used when a bookmark is unexpectedly not found.");
+            cell.imageView?.image = nil
+        }
+
+        return cell;
+    }
+}
+
+public class StubBookmarks: Bookmarks {
+    public var count: Int {
+        return 0;
+    }
+
+    public func get(index: Int) -> BookmarkItem? {
+        return nil;
+    }
+
+    public func tableDataSource() -> UITableViewDataSource {
+        return StubBookmarksUITableViewDataSource(bookmarks: self);
+    }
+}
+
+public class ArrayBookmarks: Bookmarks {
+    let bookmarks: [BookmarkItem];
+
+    init(items: [BookmarkItem]) {
+        self.bookmarks = items;
+    }
+
+    public var count: Int {
+        return self.bookmarks.count;
+    }
+
+    public func get(index: Int) -> BookmarkItem? {
+        return self.bookmarks[index];
+    }
+
+    public func tableDataSource() -> UITableViewDataSource {
+        return StubBookmarksUITableViewDataSource(bookmarks: self);
     }
 }
